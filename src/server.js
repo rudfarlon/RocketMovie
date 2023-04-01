9aaa3e3a0d46870d7287b78fdfc60e678e54407b
rud <rudfarlon@gmail.com>
2023-04-01 17:35:28 +0100

    adição de novos arquivos

--- src/server.js
+++ src/server.js
@@ -1,7 +1,10 @@
 require("express-async-errors");
+require("dotenv/config");
+
+const uploadConfig = require("./configs/upload");
+
 const migrationsRun = require("./database/sqlite/migrations");
 const AppError = require("./utils/AppError");
-const uploadConfig = require("./configs/upload");
 
 const cors = require("cors");
 const express = require("express");
@@ -34,5 +37,5 @@ app.use((error, request, response, next) => {
 
 });
 
-const PORT = 3333;
+const PORT = process.env.PORT || 3333;
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
